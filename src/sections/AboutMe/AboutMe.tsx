a66326308b3752861a0aefd5f22f89fd58b32558
pjunhua <p.junhua.2002@gmail.com>
2025-08-10 23:23:44 +0800

    Changed how AboutMe receives and displays data

--- src/sections/AboutMe/AboutMe.tsx
+++ src/sections/AboutMe/AboutMe.tsx
@@ -1,11 +1,31 @@
 import { useState } from 'react';
 import AboutMeCard from '../../components/AboutMeCard/AboutMeCard';
 import FloatingLights from '../../components/FloatingLights/FloatingLights';
+import { aboutMeArray } from './AboutMeCardData';
 
 export default function AboutMe() {
 
-    const [zIndexes, setZIndexes] = useState<number[]>([23, 22, 21]);
+    /* { length: n } defines the size of the new array, and in turn how many times to iterate the function in the second argument.
+       The function in the second argument, (_, i) => aboutMeArray.length - 1, fills the array up to 5 times due to the length.
+       The _ is a placeholder, in this case signifying we don't need it. Normally if you have an array like, 
+       const source = ['one', 'two', 'three', 'four', 'five'], then you do: Array.from(source, (value, i) => `${i}: ${value}`), the array
+       will return ['1: one', '2: two', '3: three', '4: four', '5: five']. 
+       
+       For our case, we're trying to make a descending array of numbers. For example, if aboutMeArray.length is 3, our new array will be
+       [3, 2, 1], if it's 5 then it'll be [5, 4, 3, 2, 1] due to the function, (_, i)=>aboutMeArray.length-i). */
+    const zIndexArray = Array.from({length: aboutMeArray.length}, (_, i)=>aboutMeArray.length-i);
 
+    // This useState is being referenced by the AboutMeCard's to determine their z-index
+    const [zIndexes, setZIndexes] = useState<number[]>(zIndexArray);
+
+    /* This function updates the above useState to change the order of the AboutMeCard's. This function is triggered by one of the AboutMeCard
+       when it's trying to move to the back. By updating the useState here, one AboutMeCard can indirectly affect the z-index of other
+       AboutMeCard's. 
+       
+       Let's say the current zIndexes array is [3, 2, 1], which means the first AboutMeCard's z-index is 3, second is 2, third is 1. When the
+       first AboutMeCard is triggering this to move to the back, we manipulate the array to be come (1, 3, 2), so now the first AboutMeCard is
+       1, second is 3, third is 2. Which means now the second AboutMeCard is in the front. When we repeat this again such that the zIndexes
+       array is now [2, 1, 3], now the 3rd AboutMeCard is at the front. */
     const updateZIndexes = (currentIndex: number) => {
         if (zIndexes[currentIndex] === Math.max(...zIndexes)) {
             const newArray = [...zIndexes];
@@ -18,48 +38,23 @@ export default function AboutMe() {
         }
     }
 
-    const card1 = {
-        title: 'More About Me',
-        bodyText: `Hey there, my name is Jun Hua, and I'll be ${new Date().getFullYear() - 2002} this year. I tend to be pretty reserved and may need time to open up, but I'm the type of guy that'll speak up when needed to.\n\nI enjoy experiencing new things! Since I was 16 I have worked multiple jobs whilst studying, and I always found it thrilling to have the roles reversed from a consumer standpoint, getting to learn what it's like behind-the-scenes and being a part of it. It is also through these jobs that I learnt to be more independent, be less reserved, and work better in a team.\n\nI also enjoy problem solving! From math equations when I was younger, to programming challenges or strategic games in the present.`,
-        index: 0,
-        imagePropsUrl: {
-            'topLeft': '/img/card1/abm1-tl.png',
-            'topRight': '/img/card1/abm1-tr.png',
-            'bottomLeft': '/img/card1/abm1-bl.png',
-            'bottomRight': '/img/card1/abm1-br.png'
-        }
-    }
-
-    const card2 = {
-        title: 'My Programming Background',
-        bodyText: 'I got my Diploma in Information Technology from Singapore Polytechnic back in 2022. While I have experience with other languages/frameworks like Java and React Native for Mobile App Development, I am the most comfortable with Web Development using JavaScript.\n\nHowever, I am open to learning new things, I even took the initiative to learn React in order to keep my skills updated which were dulled during my 2 years of National Service (NS). Even during my NS, I played around with making Chrome Extensions and programming with Google Sheets to solve actual problems I had in life while refreshing my programming skills.',
-        index: 1,
-        imagePropsUrl: {
-            'topLeft': '/img/card2/abm2-tl.png',
-            'topRight': '/img/card2/abm2-tr.png',
-            'bottomLeft': '/img/card2/abm2-bl.png',
-            'bottomRight': '/img/card2/abm2-br.png'
-        }
-    }
-
-    const card3 = {
-        title: 'My Hobbies',
-        bodyText: "During my free time, I enjoy watching various types of videos and playing games. The videos I like watching range from korean variety shows, to anime, to gameplay videos as well.\n\nWhen it comes to video games, I really like strategic card games (Pokemon Pocket/Master Duel), Overwatch, and gacha games. Though I haven't played much of it recently, I also enjoy single-player story games like Yakuza, Fallout, and Mafia, to name a few.\n\nEvery now and then I also play around with photo and video editing. I have the most experience with Photoshop and Filmora, but I also have experience using Vegas & Premiere Pro in the past.",
-        index: 2,
-        imagePropsUrl: {
-            'topLeft': '/img/card3/abm3-tl.png',
-            'topRight': '/img/card3/abm3-tr.png',
-            'bottomLeft': '/img/card3/abm3-bl.png',
-            'bottomRight': '/img/card3/abm3-br.png'
-        }
-    }
-
     return (
         <>
             <FloatingLights />
-            <AboutMeCard title={card1.title} nextTitle={card2.title} bodyText={card1.bodyText} zIndexObject={{ index: card1.index, array: zIndexes, zIndex: zIndexes[card1.index] }} imagePropsUrl={card1.imagePropsUrl} updateZIndex={(currentIndex: number) => { updateZIndexes(currentIndex) }} />
-            <AboutMeCard title={card2.title} nextTitle={card3.title} bodyText={card2.bodyText} zIndexObject={{ index: card2.index, array: zIndexes, zIndex: zIndexes[card2.index] }} imagePropsUrl={card2.imagePropsUrl} updateZIndex={(currentIndex: number) => { updateZIndexes(currentIndex) }} />
-            <AboutMeCard title={card3.title} nextTitle={card1.title} bodyText={card3.bodyText} zIndexObject={{ index: card3.index, array: zIndexes, zIndex: zIndexes[card3.index] }} imagePropsUrl={card3.imagePropsUrl} updateZIndex={(currentIndex: number) => { updateZIndexes(currentIndex) }} />
+            {aboutMeArray.map((card, index) => {
+
+                // Used to determine the next card's title
+                let nextCard;
+
+                // If the current card's index is 2, and the array only has 3 objects, 2+1 will loop back to 0
+                if (index + 1 >= aboutMeArray.length) {
+                    nextCard = aboutMeArray[0];
+                } else {
+                    nextCard = aboutMeArray[index + 1];
+                }
+                
+                return <AboutMeCard title={card.title} nextTitle={nextCard.title} bodyText={card.bodyText} zIndexObject={{ index: index, array: zIndexes, zIndex: zIndexes[index] }} imagePropsUrl={card.imagePropsUrl} updateZIndex={(currentIndex: number) => { updateZIndexes(currentIndex) }} />
+            })}
         </>
     )
 }
