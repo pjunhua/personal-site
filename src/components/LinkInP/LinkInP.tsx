84307b328059c8d8d0a7249475ee71e5ea123597
pjunhua <p.junhua.2002@gmail.com>
2025-08-16 07:05:39 +0800

    Added new option: brighterLink, made \n work

--- src/components/LinkInP/LinkInP.tsx
+++ src/components/LinkInP/LinkInP.tsx
@@ -4,26 +4,30 @@
     ^^ = split a link from the text to be used as the hyperlink
 
     E.g. You can learn more about it from ||this^^www.example.com|| website!
+
+    IMPORTANT: Wrap textPara in {}, otherwise any \n won't be recognized and treated as a literal string.
+    E.g. <LinkInP textPara={"Text\nNew Line"} /> as opposed to <LinkInP textPara="Text\nNew Line" />
 */
 
 import React, {forwardRef} from 'react';
 
 interface LinkInPProps {
     textPara: string,
+    brighterLink?: boolean,
     style?: React.CSSProperties,
     className?: string
 }
 
-const LinkInP = forwardRef<HTMLParagraphElement, LinkInPProps>(({textPara, style, className}, ref) => {
+const LinkInP = forwardRef<HTMLParagraphElement, LinkInPProps>(({textPara, brighterLink, style, className}, ref) => {
     const textArray = textPara.split('||');
     return (
-        <p ref={ref} style={style} className={className}>
+        <p ref={ref} style={{...style, whiteSpace: 'pre-line'}} className={className}>
             {
                 textArray.map((text) => {
                     const textSplit = text.split('^^');
                     const hasLink: boolean = textSplit.length > 1;
                     if (hasLink) {
-                        return <a href={textSplit[1]} target='_blank' rel='noopener noreferrer'>{textSplit[0]}</a>
+                        return <a href={textSplit[1]} target='_blank' rel='noopener noreferrer' style={brighterLink ? { color: '#7dcfff' } : undefined}>{textSplit[0]}</a>
                     } else {
                         return textSplit[0];
                     }
