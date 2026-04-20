53571a9142d03026f42318c61b5b0d8babcab8aa
pjunhua <p.junhua.2002@gmail.com>
2026-04-20 21:52:58 +0800

    Revise About Me card content
    Updated personal description and work experience details.

--- src/sections/AboutMe/AboutMeCardData.ts
+++ src/sections/AboutMe/AboutMeCardData.ts
@@ -12,7 +12,7 @@ export type aboutMeCard = {
 export const aboutMeArray: aboutMeCard[] = [
     {
         title: 'More About Me',
-        bodyText: `Hey there, my name is Jun Hua, and I'll be ${new Date().getFullYear() - 2002} this year. I'm a pretty quiet guy, often keeping to myself, a people observer, if you will.\n\nThat doesn't mean I won't speak up at all though, if it's a close group of friends/colleagues I can turn into a bit of a yappologist, especially if it's a common interest or when I'm teaching someone. In scenarios like work or group assignments where things need to get done, I might even take the initiative.\n\nI also like creating things that are unique to me, probably to a point that's too unhealthy though. 😅 I usually avoid the popular choice, when coding I'd rather use my unoptimised scuffed code over copy-pasting a completed product, even in games I would use an anti-meta option. It's not all bad though, this obsession also led to me making my own website and YouTube channel, which I'm still very proud of!`,
+        bodyText: `Hey there, my name is Jun Hua, and I'll be ${new Date().getFullYear() - 2002} this year.\n\nI'm known to be a quiet and stoic guy on the surface, but I open up fairly quickly and can even get quite talkative on topics of interest.\n\nI enjoy creating my own solutions for problems, like with my coding projects, as well as making my own content. I also find fulfilment in teaching others be it a new hire or a fellow student.`,
         imagePropsUrl: {
             'topLeft': `${process.env.REACT_APP_BASE_CDN_URL}/img/about_me_card/card1/abm1-tl.png`,
             'topRight': `${process.env.REACT_APP_BASE_CDN_URL}/img/about_me_card/card1/abm1-tr.png`,
@@ -22,7 +22,7 @@ export const aboutMeArray: aboutMeCard[] = [
     },
     {
         title: 'My Work & Education Background',
-        bodyText: "For a more detailed coverage, you can visit my LinkedIn ||here^^https://www.linkedin.com/in/poonjunhua/||.\n\nI got my Diploma in Information Technology from 'Singapore Polytechnic (SP)' back in 2022, and before that I graduated from the 'School of Science and Technology, Singapore (SST)', back in 2018.\n\nWhen it comes to programming I feel the most comfortable with the following as they were what I actually used to build this website: HTML, CSS, TypeScript, ReactJS, and PostgreSQL.\n\nWhile pursuing my diploma, I also worked a few part-time/temp jobs as a cashier at retail outlets like Don Don Donki, Gain City, and Cheers, for more than 3 years in total.",
+        bodyText: "For a more detailed coverage, you can visit my LinkedIn ||here^^https://www.linkedin.com/in/poonjunhua/||.\n\nI got my Diploma in Information Technology from 'Singapore Polytechnic (SP)' back in 2022, and before that I graduated from the 'School of Science and Technology, Singapore (SST)', back in 2018.\n\nWhen it comes to programming I feel the most comfortable with the following as they were what I actually used to build this website: HTML, CSS, TypeScript, ReactJS, and PostgreSQL.\n\nWhile pursuing my diploma, I have also worked a few part-time/temporary cashier jobs.",
         imagePropsUrl: {
             'topLeft': `${process.env.REACT_APP_BASE_CDN_URL}/img/about_me_card/card2/abm2-tl.png`,
             'topRight': `${process.env.REACT_APP_BASE_CDN_URL}/img/about_me_card/card2/abm2-tr.png`,
